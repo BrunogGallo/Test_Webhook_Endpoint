fa9edd07ee1ce6156875dd1e9212b2832daf7ddb
Clara Ibarzabal <claraibarzabalr@gmail.com>
2026-02-18 18:32:04 -0300

    procfile

--- endpoint.js
+++ endpoint.js
@@ -3,7 +3,7 @@ const app = express();
 app.use(express.json()); // Essential to read the JSON from Two Boxes
 
 // This is your "Endpoint"
-app.post('/two-boxes-webhook', (req, res) => {
+app.post('/test-webhook', (req, res) => {
     console.log('Received data:', req.body);
 
     // 1. Process the data (e.g., save to DB or send to Slack)
