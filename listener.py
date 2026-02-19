cf0fc6df9f890d5dc31a6a4f1dc881192c852fea
Clara Ibarzabal <claraibarzabalr@gmail.com>
2026-02-19 17:15:48 -0300

    fix

--- listener.py
+++ listener.py
@@ -7,14 +7,14 @@ app = Flask(__name__)
 @app.route('/webhook', methods=['POST'])
 def webhook():
     data = request.get_json()
-    2boxes_id = data.get('id')
+    twoboxes_id = data.get('id')
     rma_id = data.get('captured_rma')
     line_items = data.get('line_items')
 
     
     print('--- Webhook received ---')
     print('Headers:', json.dumps(dict(request.headers), indent=2))
-    print('Two Boxes ID:', 2boxes_id)
+    print('Two Boxes ID:', twoboxes_id)
     print('RMA ID:', rma_id)
     print('------------------------')
 
