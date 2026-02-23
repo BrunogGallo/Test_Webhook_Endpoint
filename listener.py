96eb22679cc718643ef5e0b510e9d0037316c3dd
Clara Ibarzabal <claraibarzabalr@gmail.com>
2026-02-23 13:42:39 -0300

    add returned items

--- listener.py
+++ listener.py
@@ -7,7 +7,7 @@ from service.mintsoftReturnService import MintsoftReturnService
 
 app = Flask(__name__)
 OrderClient = MintsoftOrderClient()
-# ReturnService = MintsoftReturnService()
+ReturnService = MintsoftReturnService()
 
 
 # Webhook endpoint – accepts POST requests
@@ -29,7 +29,6 @@ def webhook():
             "WarehouseId": 3,
             "Reference": line_items[0].get('tracking_number'),
         }
-        # add_return_items_data = ...  ## Datos de los items que integran el return
 
         for item in line_items:
             print('Storefront Order Number:', item.get('storefront_order_number'))
@@ -40,8 +39,11 @@ def webhook():
             print('------------------------')
 
         try:
-            OrderClient.create_external_return(create_external_return_data)
-            # ReturnService.add_return_items(add_return_items_data)
+            # OrderClient.create_external_return(create_external_return_data)
+            # print("Adding line items to the return...")
+            
+            ReturnService.add_return_items(49, data)
+
         except Exception as e:
             print(f"Error processing webhook: {e}")
 
