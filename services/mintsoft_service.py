3589b873e75e4866d7cbc2440466e7bc76618cd4
BrunogGallo <brunogiorgiogallo@gmail.com>
2026-03-20 17:36:30 -0300

    agrego type a endpoint de transfer stock

--- services/mintsoft_service.py
+++ services/mintsoft_service.py
@@ -340,6 +340,7 @@ class MintsoftReturnService:
                     "DestinationNameOrCode": item.get("put_away_bin"),
                     "ProductId": product_id,
                     "Quantity": item.get("quantity"),
+                    "Type": "Quarantine",
                     "Comment": "Return reallocation",
                 }
                 
