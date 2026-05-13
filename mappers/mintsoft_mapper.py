38ad5d370f237dea25359263b8a7e895462c59df
BrunogGallo <brunogiorgiogallo@gmail.com>
2026-05-13 10:49:54 -0500

    activo EW ecom y Sancia

--- mappers/mintsoft_mapper.py
+++ mappers/mintsoft_mapper.py
@@ -2,7 +2,7 @@ clients = [
   # { "m_name": "Acler", "m_id": 19, "tb_name": "acler", "tb_rma_prov": "Work Capture", "warehouse_id": 3},
   { "m_name": "Brodie", "m_id": 5, "tb_name": "brodie", "tb_rma_prov": "Work Capture", "warehouse_id": 3},
   # { "m_name": "Deiji Studios", "m_id": 10, "tb_name": "deiji studios", "tb_rma_prov": "Work Capture", "warehouse_id": 3},
-  # { "m_name": "Emilia Wickstead", "m_id": 20, "tb_name": "emilia wickstead", "tb_rma_prov": "Work Capture", "warehouse_id": 5},
+  { "m_name": "Emilia Wickstead", "m_id": 20, "tb_name": "emilia wickstead", "tb_rma_prov": "Work Capture", "warehouse_id": 5},
   { "m_name": "Emilia Wickstead", "m_id": 20, "tb_name": "emilia wickstead wholesale", "tb_rma_prov": "Work Capture", "warehouse_id": 3},
   { "m_name": "Emily Lovelock", "m_id": 18, "tb_name": "emily lovelock", "tb_rma_prov": "Work Capture", "warehouse_id": 3},
   # { "m_name": "Holiday Company", "m_id": 4, "tb_name": "holiday company", "tb_rma_prov": "Work Capture", "warehouse_id": 3},
@@ -16,7 +16,7 @@ clients = [
   { "m_name": "Nude Lucy", "m_id": 16, "tb_name": "nude lucy", "tb_rma_prov": "Work Capture", "warehouse_id": 3},
   { "m_name": "Pink City Prints", "m_id": 15, "tb_name": "pink city prints", "tb_rma_prov": "Work Capture", "warehouse_id": 3},
   # { "m_name": "Rove", "m_id": 8, "tb_name": "rove", "tb_rma_prov": "Work Capture", "warehouse_id": 3},
-  # { "m_name": "Sancia", "m_id": 23, "tb_name": "sancia", "tb_rma_prov": "Work Capture", "warehouse_id": 3},
+  { "m_name": "Sancia", "m_id": 23, "tb_name": "sancia", "tb_rma_prov": "Work Capture", "warehouse_id": 3},
   # { "m_name": "Shirty Group", "m_id": 9, "tb_name": "shirty group", "tb_rma_prov": "Work Capture", "warehouse_id": 3},
   { "m_name": "Staple & Hue", "m_id": 21, "tb_name": "staple & hue", "tb_rma_prov": "Work Capture", "warehouse_id": 5},
   { "m_name": "One Teaspoon", "m_id": 31, "tb_name": "one teaspoon", "tb_rma_prov": "Work Capture", "warehouse_id": 5}, #ECOM
